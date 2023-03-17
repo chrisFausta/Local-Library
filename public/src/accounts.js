2ce771fc3f1f01e9f5f175959683fd1429ded7e9
chrisFausta <chrisford.fausta1@gmail.com>
2023-03-17 10:37:51 -0500

    updated accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -3,13 +3,15 @@
  * @param {*} id - a string that refers the value of the id key in an account object
  * @returns the account object when given a particular ID
  */
-const findAccountById = (accounts, id) => accounts.find(account => account.id === id);
+function findAccountById(accounts, id) {
+  return accounts.find(account => account.id === id);
+}
 
 /**
  * @param {*} accounts - an array of account objects
  * @returns the list of accounts ordered by last name
  */
-const sortAccountsByLastName = accounts => {
+function sortAccountsByLastName(accounts) {
   return accounts.sort((accountA, accountB) => accountA.name.last.toLowerCase() > accountB.name.last.toLowerCase() ? 1: -1);
 }
 
@@ -19,14 +21,27 @@ const sortAccountsByLastName = accounts => {
  * @param {*} books - an array of book objects
  * @returns should return the number of times an account has created a 'borrow' record
  */
-const getTotalNumberOfBorrows = ({id}, books) => {
-
+function getTotalNumberOfBorrows({id}, books) {
   let burrowCount = 0; // a counter variable for the for loop
-  books.forEach(book => book.borrows.forEach(borrow => {
-    if (borrow.id === id) burrowCount++;
-  }))
-
+  books.forEach(book => {
+    const result = matchingIds(book.borrows, id);
+    if (result) burrowCount++;
+  });
   return burrowCount;
+}
+/**
+ * 
+ * @param {\} borrows - an array of borrow objects
+ * @param {*} id - a string that refers the value of the id key in an account object
+ * @returns an Boolean value if a borrow object has an id property with a value equal to the id parameter
+ */
+function matchingIds(borrows, id) {
+  let result = false;
+  borrows.forEach(borrow => {
+    if (borrow.id === id) result = true;
+  });
+  return result;
+
 }
 
 /**
@@ -36,13 +51,33 @@ const getTotalNumberOfBorrows = ({id}, books) => {
  * @param {*} authors - an array of author objects
  * @returns should return all of the books taken out by an account with the author embedded
  */
-const getBooksPossessedByAccount = ({id}, books, authors) => {
+function getBooksPossessedByAccount({id}, books, authors) {
   const booksPossessed = []; // create an empty array that will store the books taken out by an account and the author embedded
-  books.forEach(book => book.author = authors.filter(author => author.id === book.authorId).shift());
-  books.forEach(book => book.borrows.forEach(borrow => {
-    if (borrow.id === id && !borrow.returned) booksPossessed.push(book);
-  }))
+  books.forEach(book => {
+    book.author = authors.filter(author => author.id === book.authorId).shift();
+    const idResult = matchingIds(book.borrows, id);
+    const returnedResult = returnStatus(book.borrows, id);
+    if (idResult && !returnedResult) booksPossessed.push(book);
+  })
+  console.log("line 56:", booksPossessed)
+
   return booksPossessed;
+}
+
+/**
+ * 
+ * @param {\} borrows - an array of borrow objects
+ * @param {*} id - a string that refers the value of the id key in an account object
+ * @returns an Boolean value based on a borrow objects returned property
+ */
+function returnStatus(borrows, id) {
+  let result = false;
+  borrows.forEach(borrow => {
+    if (borrow.id === id) {
+      if (borrow.returned) result = true;
+    }
+  })
+  return result;
 }
 
 module.exports = {
